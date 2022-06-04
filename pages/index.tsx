778e795d340060c5d197892804b5811bc8b42e05
RaihanEdwin <78268341+RaihanEdwin@users.noreply.github.com>
2022-06-04 15:10:39 +0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,9 +15,9 @@ export default function Home() {
         <div className="title">Welcome to my Next JS Project</div>
         <div className="desc">
           This is my first Next JS Project. Make a simple Website with
-          TypeScript, basic routing, move to another page with "Link", Code
+          TypeScript, basic routing, move to another page with Link, Code
           Spliting and PreFetching, learn and use Global CSS and CSS Module,
-          make the Header and Footer inside the "Layout Page", Custom 404 Page
+          make the Header and Footer inside the Layout Page, Custom 404 Page
           and back to home page after 3 second, and make a Meta Data.
         </div>
       </Layout>
